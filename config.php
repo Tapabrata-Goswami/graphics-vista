39d4d87db8b9e396fd14139edbcd2b754d6a5195
Tapabrata Goswami <tapbratagoswami21@gmail.com>
2024-01-07 13:04:02 +0530

    initial

--- config.php
+++ config.php
@@ -4,8 +4,8 @@ define("base_url","https://graphicsvista.com/");
 
 // Database Configratuion
 define("DB_HOST","localhost");
-define("DB_USER","root");
-define("DB_PASS","tapa7866@A");
+define("DB_USER","u292506473_graphics_vis");
+define("DB_PASS","J5Q:ASCt/");
 define("DB_NAME","u292506473_graphics_vis");
 
 ?>
