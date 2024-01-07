840dd2b2a90adb905016aae8a2e884a89701160c
Tapabrata Goswami <tapbratagoswami21@gmail.com>
2024-01-07 18:06:07 +0530

    initial

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -5,7 +5,7 @@ session_start();
 require_once("../config.php");
 
 if(!isset($_SESSION['username'])){
-    header('Location:'.base_url.'admin-signup.php');
+    header('Location:'.base_url.'admin-signup');
 }
 ?>
 
--- admin/index.php
+++ admin/index.php
@@ -5,8 +5,8 @@ session_start();
 require_once("../config.php");
 
 if(isset($_SESSION['username'])){
-    header('Location:'.base_url.'admin/dashboard.php');
+    header('Location:'.base_url.'admin/dashboard');
 }else{
-    header('Location:'.base_url.'admin-signup.php');
+    header('Location:'.base_url.'admin-signup');
 }
 ?>
