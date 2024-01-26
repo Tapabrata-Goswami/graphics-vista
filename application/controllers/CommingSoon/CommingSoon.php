4e3d8b6fb3b8894c52f8ad1cb8a4a8258c1b5f52
Tapabrata Goswami <tapbratagoswami21@gmail.com>
2024-01-26 22:28:26 +0530

    first Commit

---
+++ application/controllers/CommingSoon/CommingSoon.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+class CommingSoon extends CI_Controller{
+    public function index(){
+        $this->load->view('templates/comming_soon');
+    }
+}
