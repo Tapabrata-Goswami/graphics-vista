ab4c3eea49233461ddb5f9081fdcadffaed0538e
Tapabrata Goswami <tapbratagoswami21@gmail.com>
2024-01-27 15:39:29 +0530

    first Commit

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,13 +49,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'CommingSoon/index';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
 
 // Users Side
-$route['/'] = 'CommingSoon/CommingSoon/index';
+// $route['/'] = ;
 
 // Admin Side
 $route['admin'] = 'auth/AdminAuth/index';
--- application/views/templates/comming_soon.php
+++ application/views/templates/comming_soon.php
@@ -14,16 +14,16 @@
         <link href="https://fonts.googleapis.com/css2?family=Tinos:ital,wght@0,400;0,700;1,400;1,700&amp;display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&amp;display=swap" rel="stylesheet" />
         <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="css/styles.css" rel="stylesheet" />
+        <link href=<?php echo base_url('assets/css/styles.css') ?> rel="stylesheet" />
     </head>
     <body>
         <!-- Background Video-->
-        <video class="bg-video" playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop"><source src="assets/mp4/bg.mp4" type="video/mp4" /></video>
+        <video class="bg-video" playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop"><source src=<?php echo base_url('assets/assets/mp4/bg.mp4'); ?> type="video/mp4" /></video>
         <!-- Masthead-->
         <div class="masthead">
             <div class="masthead-content text-white">
                 <div class="container-fluid px-4 px-lg-0">
-                    <h1 class="fst-italic lh-1 mb-4">Our Website is Coming Soon</h1>
+                    <h1 class="fst-italic lh-1 mb-4">Graphics Vista ... Coming Soon</h1>
                     <p class="mb-5">We're working hard to finish the development of this site. Sign up below to receive updates and to be notified when we launch!</p>
                     <!-- * * * * * * * * * * * * * * *-->
                     <!-- * * SB Forms Contact Form * *-->
